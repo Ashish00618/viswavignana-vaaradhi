1e50d0b5af6733c20ac4ed7b602c61c026370337
Mangam Ashish <ashishmangam2003@gmail.com>
2025-10-26 19:38:06 +0530

    fix: Correct case for Contact model require statement

--- api/controllers/contactController.js
+++ api/controllers/contactController.js
@@ -1,4 +1,4 @@
-const Contact = require('../models/Contact');
+const Contact = require('../models/contact');
 const logger = require('../utils/logger');
 
 exports.submitContact = async (req, res) => {
