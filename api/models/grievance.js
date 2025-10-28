5cbfccbeca492deb7c9a2de7f7ed52df3958d811
Mangam Ashish <ashishmangam2003@gmail.com>
2025-10-28 12:43:45 +0530

    changed the route

--- api/models/grievance.js
+++ api/models/grievance.js
@@ -1,17 +1,18 @@
+// api/models/grievance.js
 const mongoose = require('mongoose');
 
 const grievanceSchema = new mongoose.Schema({
-  fullName: { type: String, required: true },
-  mobileNumber: { type: String, required: true },
-  email: { type: String },
-  state: { type: String, required: true },
-  district: { type: String, required: true },
-  mandal: { type: String, required: true },
-  category: { type: String, required: true },
-  subcategory: { type: String, required: true },
-  description: { type: String, required: true },
-  declaration: { type: Boolean, required: true },
-  createdAt: { type: Date, default: Date.now }
+    fullName: { type: String, required: true },
+    mobileNumber: { type: String, required: true },
+    email: { type: String },
+    state: { type: String, required: true },
+    district: { type: String, required: true },
+    mandal: { type: String, required: true },
+    category: { type: String, required: true },
+    subcategory: { type: String, required: true },
+    description: { type: String, required: true },
+    declaration: { type: Boolean, required: true },
+    createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Grievance', grievanceSchema);
